459d4e792f1300144362297c014a371d1fd13a3c
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-01-24 12:11:21 +0300

    комментарии

--- sortic/p_functions.cpp
+++ sortic/p_functions.cpp
@@ -1,14 +1,14 @@
 #include "sortic.h"
 
 
-void pa(vector<int>& vector_a, vector<int>& vector_b) { //функция для выполнения команды "pa"
-	if (vector_b.size() > 0) { //проверка вектора на пустоту
+void pa(vector<int>& vector_a, vector<int>& vector_b) { //function pa
+	if (vector_b.size() > 0) { //check if vector is empty
 		push_high(vector_b[0], vector_a);
 	}
 }
 
-void pb(vector<int>& vector_a, vector<int>& vector_b) { //функция для выполнения команды "pb"
-	if (vector_a.size() > 0) { //проверка вектора на пустоту
+void pb(vector<int>& vector_a, vector<int>& vector_b) { //function pb
+	if (vector_a.size() > 0) { //check if vector is empty
 		push_high(vector_a[0], vector_b);
 	}
 }
