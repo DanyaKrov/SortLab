d20b2d9f82be337005756df253679e14e6e1220b
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-01-24 12:26:14 +0300

    Update sortic.h

--- sortic/sortic.h
+++ sortic/sortic.h
@@ -5,10 +5,18 @@
 using namespace std;
 
 
-
+//additional fucnctions
+void print_vector(const vector <int>& vector1);
+void ss(vector <int>& vector_a, vector <int>& vector_b);
 vector<int> reverse_vector(vector<int> vector1);
 void push_high(int element, vector<int>& vector);
+
+//s functions
+void sa(vector <int>& vector_a);
+void sb(vector <int>& vector_b);
+void ss(vector <int>& vector_a, vector <int>& vector_b);
+
+//p functions
 void pa(vector<int>& vector_a, vector<int>& vector_b);
 void pb(vector<int>& vector_a, vector<int>& vector_b);
-void print_vector(const vector <int>& vector1);
 void sa(vector <int>& vector_a);
