745896bd275202ae380dfeb45da3bc0d7bfe50d0
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-01-24 12:23:28 +0300

    s функции

--- sortic/print_vector.cpp
+++ sortic/print_vector.cpp
@@ -1,10 +1,10 @@
 #include "sortic.h"
 
-void print_vector(const vector <int>& vector1) //функция для вывода вектора в консоль
+void print_vector(const vector <int>& vector1) //print vector in console
 {
 	cout << "[";
 	for (int i = 0; i < vector1.size() - 1; i++) {
 		cout << vector1[i] << ", ";
 	}
-	cout << vector1[vector1.size() - 1] << "]";
+	cout << vector1[vector1.size() - 1] << "]" << endl;
 }
--- sortic/s_functions.cpp
+++ sortic/s_functions.cpp
@@ -8,3 +8,18 @@ void sa(vector <int>& vector_a)
 		vector_a[1] = temp;
 	}
 }
+
+void sb(vector <int>& vector_b)
+{
+	if (vector_b.size() > 1) {
+		int temp = vector_b[0];
+		vector_b[0] = vector_b[1];
+		vector_b[1] = temp;
+	}
+}
+
+void ss(vector <int>& vector_a, vector <int>& vector_b)
+{
+	sa(vector_a);
+	sb(vector_b);
+}
