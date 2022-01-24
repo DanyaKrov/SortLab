e0d6cd290083c6f219c002953fbe5b2311ff73bf
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-01-24 12:43:27 +0300

    функции r

---
+++ sortic.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+vector<int> reverse_vector(vector<int> vector1);
+void push_high(int element, vector<int>& vector);
+void pa(vector<int>& vector_a, vector<int>& vector_b);
+void pb(vector<int>& vector_a, vector<int>& vector_b);
+void print_vector(const vector <int>& vector1);
+void sa(vector <int>& vector_a);
+void rshift(vector <int>& mass);
+void ra(vector<int>& vector_a);
+void rb(vector<int>& vector_b);
+void rr(vector<int>& vector_a, vector<int>& vector_b);
