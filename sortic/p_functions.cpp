ec248e38cd6fb2d4ddaa37c4534bbc3ca37b05d4
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-02-02 23:44:59 +0300

    исправление p функций
    Удар. Поднятие!

--- sortic/p_functions.cpp
+++ sortic/p_functions.cpp
@@ -4,11 +4,29 @@
 void pa(vector<int>& vector_a, vector<int>& vector_b) { //function pa
 	if (vector_b.size() > 0) { //check if vector is empty
 		push_high(vector_b[0], vector_a);
+		vector <int> vector3 = {};
+		for (int i = 0; i < vector_a.size(); i++) {
+			vector3.push_back(vector_a[i]);
+		}
+		vector_a = {};
+		for (int i = 0; i < vector3.size(); i++) {
+			if (i != 1)
+				vector_a.push_back(vector3[i]);
+		}
 	}
 }
 
 void pb(vector<int>& vector_a, vector<int>& vector_b) { //function pb
 	if (vector_a.size() > 0) { //check if vector is empty
 		push_high(vector_a[0], vector_b);
+		vector <int> vector3 = {};
+		for (int i = 0; i < vector_b.size(); i++) {
+			vector3.push_back(vector_b[i]);
+		}
+		vector_b = {};
+		for (int i = 0; i < vector3.size(); i++) {
+			if (i != 1)
+				vector_b.push_back(vector3[i]);
+		}
 	}
 }
--- sortic/sortic.cpp
+++ sortic/sortic.cpp
@@ -2,9 +2,6 @@
 
 int main()
 {
-    vector <int> a = { 1, 2, 3, 4, 5 };
-    vector <int> b = { 5, 4, 3, 2, 1 };
-    rr(a, b);
-    print_vector(a);
-    print_vector(b);
+    vector <int> a;
+
 }
