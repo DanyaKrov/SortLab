2722f37855f54aa026a9cf351f46568eda738879
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-02-03 10:36:24 +0300

    Update sortic.cpp

--- sortic/sortic.cpp
+++ sortic/sortic.cpp
@@ -41,27 +41,33 @@ int len(string str)
 
 int str_to_int(string str) //convert string to integer
 {
+    bool isneg = (str[0] == '-');
     int res = 0;
     int mult = 1;
     string rev_str = "";
-    for (int i = len(str) - 1; i >= 0; i--) {
+    for (int i = len(str) - 1; i >= isneg; i--) {
         res += (str[i] - 48) * mult;
         mult *= 10;
     }
+    if (isneg)
+        res *= -1;
     return res;
+}
+void full(vector<int>& vector) {
+    string s;
+    cin >> s;
+    while (s != "!") {
+        vector.push_back(str_to_int(s));
+        cin >> s;
+    }
 }
 int main()
 {
-    string s;
     vector <int> a;
     vector <int> b;
-    cin >> s;
-    while (s != "!") {
-        a.push_back(str_to_int(s));
-        cin >> s;
-    }
+    full(a);
     int len = a.size();
-    while (b.size() < len){
+    while (b.size() < len && !(issorted(a) && issorted(b))){
         while (a[0] != min(a)) {
             ra(a);
             cout << "ra" <<endl;
@@ -69,10 +75,12 @@ int main()
         pb(a, b);
         cout << "pb" <<endl;
     }
-    for (int i = 0; i < len; i++) {
+    len = b.size();
+    int len2 = a.size();
+    for (int i = 0; i < mod(len - len2); i++) {
         pa(a, b);
         cout << "pa" <<endl;
     }
-    print_vector(a);
-    print_vector(b);
+    print_vector(a, "a");
+    print_vector(b, "b");
 }
